02c8c0dc5f35bd6eb75cf2541aed647a78cbec66
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-02-21 16:32:21 -0600

    updated requirements with necessary modules to run an NLP classification model, web server resonse was accurate

--- server.py
+++ server.py
@@ -28,3 +28,40 @@ def index():
     for x in range(3):
         result = str(random.randint(1,7)) + str(random.randint(1,7)) + str(random.randint(1,7))
         return render_template('dice.html', name='Brandon', results=result)
+
+# Define a route that will display a form
+@app.route('/my-first-form')
+def my_first_form():
+    return render_template('my-first-form.html')
+
+# Define a route that handles the form submission:
+from flask import request
+
+@app.route('/make-greeting', methods=['POST'])
+def handle_form_submission():
+    name = request.form['name']
+    title = request.form['title']
+
+    greeting = 'Hello, '
+
+    if title != '':
+        greeting += title + ' '
+    
+    greeting += name + '!'
+
+    return render_template('greeting-result.html', greeting=greeting)
+
+# Define a route that will display a form
+@app.route('/message-form')
+def message_form():
+    return render_template('message.html')
+
+@app.route('/message', methods=['POST'])
+def handle_form_input():
+    from model import predict
+    
+    textmsg = request.form['textmsg']
+    
+    result = predict(textmsg)
+
+    return render_template('text-result.html', textmsg=textmsg, result=result)
