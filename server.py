9dd8b2e7fee3188c295453dddc4f33e993f57b14
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-02-21 13:03:27 -0600

    added html to flask apps

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask import render_template
+
+# The following terminal commands runs the app
+'''
+export FLASK_APP=server.py
+export FLASK_ENV=development
+flask run
+'''
 
 # creates flask application, use to define routes
 # or urls within the webserver
 app = Flask(__name__)
 
 # run the index function, the return statement
-# will be what the web server return to the browser
+# will be what the web server return to the browser as html
 @app.route('/')
 def index0():
-    return 'Hello, World!'
+    return render_template('index.html', name='Brandon')
 
-# 3 random numbers between 1 and 6 are shown
+# 3 random numbers between 1 and 6 are shown as html
 @app.route('/roll-dice')
 def index():
     '''
@@ -18,4 +26,5 @@ def index():
     '''
     import random
     for x in range(3):
-        return str(random.randint(1,7)) + str(random.randint(1,7)) + str(random.randint(1,7))
+        result = str(random.randint(1,7)) + str(random.randint(1,7)) + str(random.randint(1,7))
+        return render_template('dice.html', name='Brandon', results=result)
