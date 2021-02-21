5c0bde6a7da40469be24a0f451b0ca67b4f0aa2f
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-02-21 12:31:44 -0600

    commented explanation in code

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
+# creates flask application, use to define routes
+# or urls within the webserver
 app = Flask(__name__)
 
+# run the index function, the return statement
+# will be what the web server return to the browser
 @app.route('/')
 def index0():
     return 'Hello, World!'
 
+# 3 random numbers between 1 and 6 are shown
 @app.route('/roll-dice')
 def index():
     '''
